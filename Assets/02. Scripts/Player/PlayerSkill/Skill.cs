2818ad038fa093c8028fab1ea7f73c19d21162bf
이승빈 <144720218+Pyorang@users.noreply.github.com>
2025-11-18 03:01:21 +0900

    Feat : 대쉬공격 쿨타임 따로 적용

--- Assets/02. Scripts/Player/PlayerSkill/Skill.cs
+++ Assets/02. Scripts/Player/PlayerSkill/Skill.cs
@@ -1,5 +1,8 @@
+using NUnit.Framework;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public abstract class Skill : MonoBehaviour
 {
@@ -9,23 +12,32 @@ public abstract class Skill : MonoBehaviour
 
     [Header("재사용 대기 시간")]
     [Space]
-    protected static bool _isAttacking = false;
+    protected static bool s_isAttacking = false;
     [SerializeField] private float _waitTimeToControl = 1f;
+
+    [Header("재사용 대기 시간 이미지")]
+    [Space]
+    [SerializeField] protected Image _coolTimeImage;
+    protected static List<Image> s_images = new List<Image>();
 
     protected PlayerMove _playerMove;
     protected Animator _animator;
 
     private void Awake()
     {
+        s_images.Add(_coolTimeImage);
         _playerMove = GetComponent<PlayerMove>();
         _animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        if(Input.GetKeyDown(_pressButton) && !_isAttacking)
+        if(Input.GetKeyDown(_pressButton) && !s_isAttacking)
         {
-            _isAttacking = true;
+            s_isAttacking = true;
+
+            StartCoroutine(UpdateCoolTimeImage());
+
             _playerMove.SetMovementLock(true);
             Execute();
             StartCoroutine(WaitAttackTime());
@@ -35,8 +47,36 @@ public abstract class Skill : MonoBehaviour
     protected IEnumerator WaitAttackTime()
     {
         yield return new WaitForSeconds(_waitTimeToControl);
-        _isAttacking = false;
+        s_isAttacking = false;
         _playerMove.SetMovementLock(false);
+    }
+
+    protected IEnumerator UpdateCoolTimeImage()
+    {
+        float startTime = Time.time;
+        float endTime = startTime + _waitTimeToControl;
+        float coolDownDuration = _waitTimeToControl;
+
+        foreach(Image image in s_images)
+        {
+            image.fillAmount = 1;
+        }
+
+        while (Time.time < endTime)
+        {
+            float elapsedTime = Time.time - startTime;
+            float progress = elapsedTime / coolDownDuration;
+            foreach (Image image in s_images)
+            {
+                image.fillAmount = 1f - progress;
+            }
+            yield return null;
+        }
+
+        foreach (Image image in s_images)
+        {
+            image.fillAmount = 0;
+        }
     }
 
     public abstract void Execute();
